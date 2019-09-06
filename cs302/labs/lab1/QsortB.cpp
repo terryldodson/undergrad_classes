be40ddb761358658e35441216cca9abb2e14d8da
Terryl Dwaine Dodson <tdodson3@hydra3.eecs.utk.edu>
2019-09-05 22:57:19 -0400

    modified:   QsortA 	modified:   QsortB.cpp 	modified:   makefile 	lab1A.tar

--- cs302/labs/lab1/QsortB.cpp
+++ cs302/labs/lab1/QsortB.cpp
@@ -1,10 +1,62 @@
 
 // include header files needed
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-class data { ... }
+class data {
+ public:
+    // add operator< using lastname, firstname, phone number
 
-void printlist(...) { ... }
+    friend istream & operator>>(istream &, data &);
+    friend ostream & operator<<(ostream &, const data &);
+
+    bool operator<(const data &) const;
+
+  private:
+    string firstname;
+    string lastname;
+    string phonenum;
+}
+
+istream & operator>>(istream &in, data &r) {
+  // write this to read data object data
+  in >> r.firstname >> r.lastname >> r.phonenum;
+  return in;
+}
+
+ostream & operator<<(ostream &out, const data &r) {
+  // write this to write data object data
+  out << setw(24) << left << r.lastname + " "  + r.firstname << r.phonenum << "\n";
+  return out;
+}
+
+bool data::operator<(const data &rhs) const {
+    /*compare last name*/
+    if(lastname != rhs.lastname)
+        return lastname < rhs.lastname;
+
+    /*compare first name*/
+    if(firstname != rhs.firstname)
+        return firstname < rhs.firstname;
+
+    /*compare phone number*/
+    return phonenum < rhs.phonenum;
+}
+
+template <typename T>
+void printlist(T i, T p) {
+  // template based iterator code for printing data to stdout
+  while(i != p) {
+    cout << *i;
+    i++;
+  }
+  return;
+}
 
 template <typename Tdata>
 void quicksort(...) { 
@@ -29,7 +81,7 @@ int main(int argc, char *argv[]) {
   // add selection between sorting options
 
   // option -stl
-  std::sort(A.begin(), A.end();
+  std::sort(A.begin(), A.end());
 
   // option -rpivot
   int N = (int)A.size();
