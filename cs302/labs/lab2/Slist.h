af4d0d73ff315968a956225fe62d38ea19242880
Terryl Dwaine Dodson <tdodson3@hydra4.eecs.utk.edu>
2019-09-16 11:06:31 -0400

    modified:   Slist.h 	new file:   Slist_usage 	modified:   Slist_usage.cpp 	modified:   makefile 	modified:   sslist_usage

--- cs302/labs/lab2/Slist.h
+++ cs302/labs/lab2/Slist.h
@@ -2,21 +2,39 @@
 #define SLIST_H
 
 // include header file(s) needed
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stdlib.h>
 
-// template <typename T>
+using namespace std;
+
 template <class T>
 class slist {
   private:
     struct node {
       node() { data = T(); next = NULL; }
-      // add node(const T &key) { write this }
+      node(const T &key) {data = key; next = NULL;}
       // add overloaded operator< code
+	  bool operator< (const node &rhs) const { return data < rhs.data; }
 
       T data;
       node *next;
     };
 
-   // add class sptr { write this for node data }
+// add class sptr
+class sptr {
+	public:
+    	sptr(node *_ptr=NULL) { ptr = _ptr; }
+    	bool operator< (const sptr &rhs) const {
+      		return *ptr < *rhs.ptr;
+    	}
+    	operator node * () const { return ptr; }
+  	private:
+  		node *ptr;
+};
 
   public:
 	class iterator {
@@ -25,6 +43,7 @@ class slist {
 	    T & operator*() { return p->data; }
 	    iterator & operator++() { p = p->next; return *this; }
 	    bool operator!=(const iterator & rhs) const { return p != rhs.p; }
+
 
 	  private:
 	    iterator(node *n_p) { p = n_p; }
@@ -76,10 +95,39 @@ void slist<T>::push_back(const T &din) {
 template <typename T>
 void slist<T>::sort(const string &algname) {
   // determine number of list elements
+	int num = 0;
+	node *p;
+	
+	//get size of list
+	p = head;
+	while(p->next != NULL) {
+		num++;
+		p = p->next;
+	}
+
   // set up smart pointer array called Ap
+	vector<sptr> Ap(num);
+	
+	p = head;
+	for(int i = 0; i < Ap.size(); i++) {
+		Ap[i] = p->next;
+		p = p->next;
+	}
   // if quicksort, apply std::sort(...)
+	if(algname == "quicksort")
+		std::sort(Ap.begin(), Ap.end());
   // if mergesort, apply std::stable_sort(...)
-  // use sorted Ap array to relink list 
+	if(algname == "mergesort")
+		std::stable_sort(Ap.begin(), Ap.end());
+  // use sorted Ap array to relink list
+	p = head;
+	for(int i = 0; i < Ap.size(); i++) {
+		p->next = Ap[i];
+		p = p->next;
+	}
+
+  //set end of list to NULL so we dont loop back to a random spot
+	p->next = NULL;
 }
 
 #endif // SLIST_H
--- cs302/labs/lab2/Slist_usage.cpp
+++ cs302/labs/lab2/Slist_usage.cpp
@@ -1,4 +1,11 @@
-// include header file(s) needed
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+
 using namespace std;
 
 #include "Slist.h"
@@ -7,14 +14,70 @@ using namespace std;
 // and output operators to store, set and print a 
 // line number identifier called ID
 
-class data { copy from QsortA.cpp };
-istream & operator>> (...) { copy from QsortA.cpp }
-ostream & operator<< (...) { copy from QsortA.cpp }
+class data {
+  public:
+    // add operator< using lastname, firstname, phone number
 
-void printlist(...) { copy from QsortA.cpp }
+    friend istream & operator>>(istream &, data &);
+    friend ostream & operator<<(ostream &, const data &);
+
+    bool operator<(const data &) const;
+
+  private:
+    string firstname;
+    string lastname;
+    string phonenum;
+	int ID;
+};
+
+istream & operator>>(istream &in, data &r) {
+  // write this to read data object data
+  in >> r.firstname >> r.lastname >> r.phonenum;
+  static int id;
+  r.ID = ++id;
+  return in;
+}
+
+ostream & operator<<(ostream &out, const data &r) {
+  // write this to write data object data
+  out << " " << setw(24) << left << r.lastname + " "  + r.firstname << r.phonenum << setw(9) << right << r.ID << "\n";
+  return out;
+}
+
+bool data::operator<(const data &rhs) const {
+    /*compare last name*/
+    if(lastname != rhs.lastname)
+        return lastname < rhs.lastname;
+
+    /*compare first name*/
+    if(firstname != rhs.firstname)
+        return firstname < rhs.firstname;
+
+    /*compare phone number*/
+    return phonenum < rhs.phonenum;
+}
+
+template <typename T>
+void printlist(T i, T p) {
+  // template based iterator code for printing data to stdout
+  while(i != p) {
+    cout << *i;
+    ++i;
+  }
+  return;
+}
 
 int main(int argc, char *argv[]) {
   // copy command-line check from QsortB.cpp,
+	if(argc != 2) { 
+		cerr << "usage: ./Slist_usage -quicksort|mergesort < file.txt" << endl;
+        return 1;	
+	}
+		
+	if(strcmp(argv[1], "-quicksort") != 0 && strcmp(argv[1], "-mergesort") != 0) {
+    	cerr << "usage: ./Slist_usage -quicksort|mergesort < file.txt" << endl;
+    	return 1;
+    }
   // then modify to apply to this assignment
 
   slist<data> A;
@@ -23,7 +86,12 @@ int main(int argc, char *argv[]) {
   while (cin >> din)
     A.push_back(din);
 
-  A.sort(command line mode option);
+ // A.sort(command line mode option);
+ // used sort from Slist.h
+	if(strcmp(argv[1], "-quicksort") == 0)
+		A.sort("quicksort");
+	else
+		A.sort("mergesort");
 
   printlist(A.begin(), A.end());
 }
