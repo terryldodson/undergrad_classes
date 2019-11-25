e08802eef01480086b87831683c948693e4e5c9a
Terryl Dwaine Dodson <tdodson3@hydra4.eecs.utk.edu>
2019-11-25 10:39:16 -0500

    modified:   Sptrsort.h

--- cs302/labs/lab8/Sptrsort.h
+++ cs302/labs/lab8/Sptrsort.h
@@ -1,11 +1,35 @@
-WRITE THIS BASED ON SMART POINTER CLASS
+//WRITE THIS BASED ON SMART POINTER CLASS
+using namespace std;
 
+template <typename T>
 class sptr {
-  COPY FROM CLASS HANDOUT
-};
+  public:
+    sptr(T *_ptr=NULL) { ptr = _ptr; }
+    bool operator< (const sptr &rhs) const {
+      return *ptr < *rhs.ptr;
+    }
+    operator T * () const { return ptr; }
+  private:
+    T *ptr;
+}; 
 
 template <typename T>
 void sptrsort(std::vector<T *> &A) {
-  USE std::sort on SPTR VERSION OF A
-  NOTE: A is vector of pointers (T *)
+	size_t a_sz, i;
+	vector< sptr<T> > ptrs;
+
+	//vector of smart pointers
+	a_sz = A.size();
+	ptrs.resize(a_sz);
+
+	//fill with values from "A"
+	for(i = 0; i < a_sz; i++)
+		ptrs[i] = sptr<T> (A[i]);
+	
+	//Utilize Introsort
+	sort(ptrs.begin(), ptrs.end());
+
+	//store sorted values back into "A"
+	for(i = 0; i < a_sz; i++)
+		A[i] = ptrs[i];
 }
