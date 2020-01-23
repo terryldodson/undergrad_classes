f44c4f14e75a6d004a2829786a45662fd4846333
Terryl Dodson <terryldodson@Terryls-MacBook-Pro.local>
2020-01-22 20:49:53 -0500

    modified:   2.sql

--- cs465/labs/hw1/2.sql
+++ cs465/labs/hw1/2.sql
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-select * from Room where (type = 'double' or type = 'family') and price < 40 ORDER BY price;
-=======
 select * FROM Room WHERE (type = 'double' OR type = 'family') AND price < 40.00 ORDER BY price;
->>>>>>> deb3d5fa4889096e1f890809db116c6d588bcef8
