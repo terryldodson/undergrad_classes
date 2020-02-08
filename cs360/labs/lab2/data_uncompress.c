dcf7fd739fa00203f4e31a345789f56d87f7b260
Terryl Dwaine Dodson <tdodson3@hydra1.eecs.utk.edu>
2020-02-08 12:05:32 -0500

    modified:   data_uncompress 	modified:   data_uncompress.c

--- cs360/labs/lab2/data_uncompress.c
+++ cs360/labs/lab2/data_uncompress.c
@@ -5,14 +5,13 @@
 
 int main(int argc, char **argv) {
 	int c;
-	int num_doubles;
-	int num_ints;
-	int word_count, word_size;
+	int num_of_doubles;
+	int num_of_ints;
+	int wc, ws; //word count and word size
 	int letter = 0;
 	int i, j;
-	//int counter = 0;
 
-	c= getchar();
+	c = getchar();
 
 	while(c != EOF) {
 
@@ -22,12 +21,12 @@ int main(int argc, char **argv) {
 		c = getchar();
 
 		if(c == 'd') {
-			num_doubles = getchar();
-			num_doubles++;
+			num_of_doubles = getchar();
+			num_of_doubles++;
 			double dbuffer[300];
 			int nread;
-			nread = fread(&dbuffer, sizeof(double), num_doubles, stdin);
-			if(nread != num_doubles) {
+			nread = fread(&dbuffer, sizeof(double), num_of_doubles, stdin);
+			if(nread != num_of_doubles) {
 				printf("Input error: not enough doubles\n");
 				exit(1);
 			}
@@ -35,12 +34,12 @@ int main(int argc, char **argv) {
 		}
 
 		if(c == 'i') {
-			num_ints  = getchar();
-			num_ints++;
+			num_of_ints  = getchar();
+			num_of_ints++;
 			int ibuffer[300];
 			int tmp;
-			tmp = fread(&ibuffer, sizeof(int), num_ints, stdin);
-			if(tmp != num_ints) {
+			tmp = fread(&ibuffer, sizeof(int), num_of_ints, stdin);
+			if(tmp != num_of_ints) {
 				printf("Input error: not enough ints\n");
 				exit(1);
 			}
@@ -48,14 +47,14 @@ int main(int argc, char **argv) {
 		}
 
 		if(c == 's') {
-			word_count = getchar();
-			word_count += 1;
+			wc = getchar();
+			wc += 1;
 
-			for(i = 0; i < word_count; i++) {
-				word_size = getchar();
-				word_size += 1;
+			for(i = 0; i < wc; i++) {
+				ws = getchar();
+				ws += 1;
 				
-				for(j = 0; j < word_size; j++) {
+				for(j = 0; j < ws; j++) {
 					letter = getchar();
 					if(isprint(letter))
 						printf("%c", letter);
@@ -72,76 +71,7 @@ int main(int argc, char **argv) {
 		
 		else if(c == EOF)
 			break;
-		
-/*		else if(c != 'n' && c != 'i' && c != 's' && c != 'd') {
-			printf("Input error: bad type\n");
-			exit(1);
-		}*/
-		
-		//counter++;
 	}
 
 	return 0;
 }
-		/*	int c, i, nread, j, letter;
-			unsigned char n, size;
-			char cbuffer[300];
-			int ibuffer[300];
-			double dbuffer[300];
-
-			c = getchar();
-
-			while(c != EOF) {
-
-			if(c != 'n' && c != 's' && c != 'i' && c != 'd') 
-			fprintf(stderr, "Input error: bad type\n");
-
-			switch(c) {
-		//	printf(c);
-		case 'n':
-		printf("\n");
-		break;
-		case 's':
-		n = getchar();
-		n++;
-
-		for(i=0; i <n; i++) {
-		size = getchar();
-		size++;
-
-		for(j = 0; j < size; j++) {
-		letter = getchar();
-		if(isprint(letter))
-		printf("%c", letter);
-		}
-		}
-
-		if(i != n-1)
-		printf(" ");
-
-		//	size = getchar();
-		//	fread(cbuffer, size + sizeof(char), n+1, stdin);
-
-		break;
-		case 'i':
-		n = getchar();
-		n++;
-		fread(&ibuffer, sizeof(int), n, stdin);
-		printf("%d ", *ibuffer);
-		//printf("dogs");
-		break;
-		case 'd':
-		n = getchar();
-		n++;
-		fread(&dbuffer, sizeof(double), n, stdin);
-		printf("%.10lg ", *dbuffer);
-		//printf("jellybean");
-		break;
-		}
-
-		c = getchar();
-
-		if(c == EOF)
-		break;
-		} */
-//	}
