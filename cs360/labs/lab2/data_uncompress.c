67fd8760e065389a1db5159fc51aaa3b1dd0245d
Terryl Dwaine Dodson <tdodson3@hydra3.eecs.utk.edu>
2020-02-09 17:42:21 -0500

    modified:   data_uncompress 	modified:   data_uncompress.c

--- cs360/labs/lab2/data_uncompress.c
+++ cs360/labs/lab2/data_uncompress.c
@@ -18,65 +18,106 @@ int main(int argc, char **argv) {
 
 	while((c = getchar()) != EOF) {
 
-		if(c != 'n' && c != 's' && c != 'i' && c != 'd')
-			printf("Input error: bad type\n");
+		if(c != 'n' && c != 's' && c != 'i' && c != 'd') {
+			fprintf(stderr, "Input error: bad type\n");
+			exit(1);
+		}
 
 		//c = getchar();
 
-		if(c == 'd') {
+		else if(c == 'd') {
 			num_of_doubles = (int) getchar();
+			
+			if(num_of_doubles == -1) {
+				fprintf(stderr, "Input error: no size\n");
+				exit(1);
+			}
+
 			num_of_doubles++;
 			int nread;
 			nread = fread(dbuffer, sizeof(double), num_of_doubles, stdin);
+			
 			if(nread != num_of_doubles) {
-				printf("Input error: not enough doubles\n");
+				fprintf(stderr, "Input error: not enough doubles\n");
 				exit(1);
 			} //end of inner if
+
 			for(i = 0; i < num_of_doubles; i++) {
 				printf("%.10lg", dbuffer[i]);
+				printf(" ");
 			}//end of for loop
 		} //end of if
 
 		else if(c == 'i') {
 			num_of_ints = (int) getchar();
+			
+			if(num_of_ints == -1) {
+				 fprintf(stderr, "Input error: no size\n");
+				exit(1);
+			}
+
 			num_of_ints++;
 			int tmp;
+
 			tmp = fread(ibuffer, sizeof(int), num_of_ints, stdin);
+			
 			if(tmp != num_of_ints) {
-				printf("Input error: not enough ints\n");
+				fprintf(stderr, "Input error: not enough ints\n");
 				exit(1);
 			} //end of inner if
+
 			for(i = 0; i< num_of_ints; i++) {
-				printf("%d ", ibuffer[i]);
+				printf("%d", ibuffer[i]);
+				printf(" ");
 			}
 		} //end of if
 
 		else if(c == 's') {
 			wc = (int) getchar();
+
+			if(wc == -1) {
+				fprintf(stderr, "Input error: no size\n");
+				exit(1);
+			}
+
 			wc += 1;
 
 			for(i = 0; i < wc; i++) {
 				ws = (int) getchar();
 				ws += 1;
 				
-				fread(cbuffer, sizeof(char), ws, stdin);
-				cbuffer[ws-1] = '\0';	
-				//for(j = 0; j < ws; j++) {
-				printf("%s", cbuffer);
-				//} //end of inner for loop
+				int tmp2;
+				tmp2 = fread(cbuffer, sizeof(char), ws, stdin);
 				
-				printf(" ");
+				if(tmp2 != ws) {
+					fprintf(stderr, "Input error: not enough chars\n");
+					exit(1);
+				}
+
+				if(tmp2 == 0) {
+					fprintf(stderr, "Input error: no string size\n");
+					exit(1);
+				}
+
+				cbuffer[ws] = '\0';	
+				
+				printf("%s", cbuffer);
+			
+				if(i != wc - 1)
+					printf(" ");
 			} //end of outer for loop
-		}//end of if
+		}//end of else if
 		
-		if(c == 'n') {
+		else if(c == 'n') {
 			printf("\n");
-		} //end of else if
+		} //end of if
 		
+		//else if(c != 'n' && c != 's' && c != 'i' && c != 'd')
+		//	printf("Input error: bad type\n");
+
 		else if(c == EOF) {
 			break;
 		} //end of else if
-		//c = (int) getchar();
 	} //end of while
 
 	return 0;
