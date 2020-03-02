43bf7d2c7aa7d15f1dd0ed11a850247ba74a3a87
Terryl Dwaine Dodson <tdodson3@hydra3.eecs.utk.edu>
2020-03-01 23:15:11 -0500

    modified:   tarx.c

--- cs360/labs/lab5b/tarx.c
+++ cs360/labs/lab5b/tarx.c
@@ -13,7 +13,7 @@
 #include <time.h>
 
 typedef struct directory {
-	char* filename;
+	char filename[256];
 	int mode;
 	long mod_time;
 	struct utimbuf* ubuf;
@@ -30,12 +30,14 @@ int main(int argc, char *argv[]) {
 	FILE *f;
 
 	int size;
-	char name[258];
+	char name[256];
 	long inode;
 	int mode;
 	long mod_time;
 
-	int serror, nerror, ierror, merror, moderror, berror;
+	long filesize;
+
+	int serror, nerror, ierror, merror, moderror, berror, ferror, cerror;
 
 	if(argc != 1) {
 		fprintf(stderr, "usage: tarc directory\n");
@@ -45,19 +47,23 @@ int main(int argc, char *argv[]) {
 	inodes = make_jrb();
 
 	while(1) {
-		serror = fread(&size, 4, 1, stdin);
-		printf("Size: %s\n", &size);
-		//name[size] = '\0';
-		nerror = fread(name, sizeof(char), strlen(name), stdin);
+		serror = fread(&size, sizeof(int), 1, stdin);
+		if(serror != 1 && feof(stdin))
+			exit(1);
+		printf("Size: %d\n", &size);
+		
+		nerror = fread(&name, size, 1, stdin);
 		printf("Name: %s\n", &name);
-		ierror = fread(&inode, 8, 1, stdin);
+		ierror = fread(&inode, sizeof(long), 1, stdin);
 		printf("Inode: %ld\n", &inode);
 
 		printf("%d %d %d", serror, nerror, ierror);
-		if(serror != 1 || nerror != 1 || ierror != 1) {
+		if((nerror == 0 || ierror == 0) && feof(stdin)) {
 			fprintf(stderr, "1nothing to read\n");
 			exit(1);
 		}
+
+		name[size] = '\0';
 
 		tmp = jrb_find_int(inodes, inode);
 		if(tmp == NULL) {
@@ -72,25 +78,57 @@ int main(int argc, char *argv[]) {
 			jrb_insert_int(inodes, inode, new_jval_s(strdup(name)));
 
 			if(S_ISDIR(mode)) {
+				d = (Dir*) malloc(sizeof(Dir));
+				d -> mode = mode;
+				d-> mod_time = mod_time;
+				strcpy(d->filename, name);
 				mkdir(name, S_IRWXU | S_IRWXG | S_IRWXO);
 				dll_append(directories, new_jval_v((void*) d));	
 			} else {
+				ferror = fread(&filesize, 8, 1, stdin);
+				if(ferror != 1) {
+					fprintf(stderr, "nothing to print\n");
+					exit(1);
+				}
+
+				char* buffer; 
+				buffer = (char*)malloc(sizeof(char) * filesize);	
+				berror = fread(buffer, filesize, 1, stdin);
+				
 				f = fopen(name, "w");
-				char* buffer[size]; 
+				if(f == NULL) {
+					fprintf(stderr, "can't open file %s\n", name);
+				}
+				
+				fwrite(buffer, filesize, 1, f);
 				//serror = fread(&size, 4, 1, stdin);
-				berror = fwrite(&buffer, sizeof(char*), 1, f);
 				//printf("Mode: %d\n", mode);
 				//printf("Mod time %ld\n", mod_time);
 				//printf("Name: %s\n", name);
 				fclose(f);
 
-				//printf("pizza");
-				printf("serror: %d berror: %d\n", serror, berror);
-				//berror = 1;
-				if(serror != 1 || berror != 1) {
-					fprintf(stderr, "3nothing to read\n");
+				//sets the files mode
+				chmod(name, mode);
+				
+				//updating mod_time
+				struct timeval time_info[2];
+				time_info[0].tv_sec = time(NULL);
+				time_info[0].tv_usec = 0;
+				time_info[1].tv_sec = mod_time;
+				time_info[1].tv_usec = 0;
+
+				cerror = utimes(name, time_info);
+				if(cerror != 0) {
+					fprintf(stderr, "couldn't complete utimes\n");
 					exit(1);
 				}
+				//printf("pizza");
+				/*printf("serror: %d berror: %d\n", serror, berror);
+				//berror = 1;
+				if(serror == 0 || berror == 0) {
+					fprintf(stderr, "3nothing to read\n");
+					exit(1);
+				}*/
 			}//end of if else	 
 			//printf("%s", "Terryl");
 		} //end of if
@@ -100,9 +138,9 @@ int main(int argc, char *argv[]) {
 //			link(&inode, inodes);				
 //		}
 //
-		dll_traverse(tmp1, directories) {
+		/*dll_traverse(tmp1, directories) {
 			//set the mode
-			chmod(d->filename, mode);
+			//chmod(d->filename, mode);
 			//setting mod_time
 			//printf("pizza");
 			//acces modification time
@@ -118,6 +156,6 @@ int main(int argc, char *argv[]) {
 			printf("Name: %s\n", name);
 			//fwrite(name, sizeof(char), strlen(name), f);
 		} //end of traverse	
-		printf("pizza");
+		printf("pizza");*/
 	} //end of while
 } //end of main
