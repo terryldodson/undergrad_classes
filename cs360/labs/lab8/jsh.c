9f4ca0a3190aa55dd4085da0d532c5c3593dc5f6
Terryl Dwaine Dodson <tdodson3@hydra2.eecs.utk.edu>
2020-04-08 20:54:31 -0400

    modified:   jsh.c

--- cs360/labs/lab8/jsh.c
+++ cs360/labs/lab8/jsh.c
@@ -12,7 +12,7 @@
 #include "fields.h"
 #include "jrb.h"
 
-void pipe(JRB processes, IS is->fields);
+void pipe_funct(JRB processes, IS is);
 
 int main (int argc, char  *argv[]) {
 	char* prompt;
@@ -136,126 +136,71 @@ int main (int argc, char  *argv[]) {
 
 					//if | is found then we pipe
 					else if(strcmp(is->fields[i], "|") == 0) {
+						pipe_funct(processes, is);
+					}//end of else if
+				}//end of for
 
+				newargv[is->NF] = NULL;
 
-						//call pipe to set up two file descriptors
-						i = pipe(pipefd);
+				execvp(newargv[0], newargv);
 
-						//if it is unable to create pipe for interprocess communication
-						//print error and exit
-						if(i < 0) {
-							perror(is->fields[i+1]);
-							exit(1);
-						}//end of if
+				perror(newargv[0]);
+				exit(1);
+			} else {
 
-						//fork to create a new process and set to fv
-						fv = fork();
+				//insert value returned from fork into jrb tree
+				jrb_insert_int(processes, fv, new_jval_i(fv));
 
-						//insert into jrb
-						jrb_insert_int(processes, fv, new_jval_i(fv));
+				if(!amp) { 
+					/*while loop to look through jrb tree until I find process I want which is fv*/
+					/*keep calling wait until the fork (fv) is equal to what wait returns*/
+					/*once I find one equal just break*/
+					while(1) {		
 
-						//check to see if fv is equal to 0
-						if(fv == 0) {
+						pid = wait(&stat_loc);
 
-							//check to see if it was successfully opened
-							//if not, we check to see if it was able to read from file
-							//if not, print error and exit
-							//then close file
-							//if(fd1 < 0) {
-							if(dup2(pipefd[1], 1) != 1) {
-								perror(is->fields[i+1]);
-								exit(1);
-							}//end of if
-							close(pipefd[1]);
-							close(pipefd[0]);
-							execlp(newargv, newargv[0]);
-							exit(1);
-							//}//end of if
+						//once I find the pid value, search for it in the tree
+						//set to pid_num 
+						JRB pid_num;
+						pid_num = jrb_find_int(processes, pid);
 
-							//check to see if it was successfully opened
-							//if not, check to see if it was able to write to file
-							//if not, print error and exit
-							//then close file
-							//if(fd2 < 0) {
-							if(dup2(pipefd[0], 0) != 0) {
-								perror(is->fields[i+1]);
-								exit(1);
-							}//end of if
-							close(pipefd[1]);
-							close(pipefd[0]);
-							execlp(newargv, newargv[0]);
-							exit(1);
-							//}//end of if
+						//if I can't find pid_num in tree, don't do anything
+						if(pid_num == NULL) {
 
-							//check to see if it was successfully opened
-							//if not, check to see if it was able to write to file
-							//if not, print error and exit
-							//then close file
-							if(fd3 < 0) {
-								if(dup2(fd3, 1) != 1) {
-									perror(is->fields[i+1]);
-									exit(1);
-								} //end of if
-								close(fd3);
-							} //end of if
-						}//end of else if
-					}//end of for
+						}//ends of if
 
-					newargv[is->NF] = NULL;
+						//if I do find pid_num, delete node
+						jrb_delete_node(pid_num);
 
-					execvp(newargv[0], newargv);
+						//if pid is equal to fv then break
+						if(pid == fv) {
+							break;
+						}//end of inner if
+					} //end of while
+				} //end of outer if
+			} //end of else
 
-					perror(newargv[0]);
-					exit(1);
-				} else {
+			//free newargv
+			free(newargv);
+		} //end of while
+	} //end of if
 
-					//insert value returned from fork into jrb tree
-					jrb_insert_int(processes, fv, new_jval_i(fv));
+	/*traverse jrb tree and call wait*/
+	jrb_traverse(p, processes) {
+		wait(&stat_loc);
+	} //end of jrb_traverse 
 
-					if(!amp) { 
-						/*while loop to look through jrb tree until I find process I want which is fv*/
-						/*keep calling wait until the fork (fv) is equal to what wait returns*/
-						/*once I find one equal just break*/
-						while(1) {		
+	//free jrb tree
+	jrb_free_tree(processes);
+} //end of main
 
-							pid = wait(&stat_loc);
+void pipe_funct(JRB processes, IS is) {
+	int pipefd[2], i, fv;
+	char** newargv;
 
-							//once I find the pid value, search for it in the tree
-							//set to pid_num 
-							JRB pid_num;
-							pid_num = jrb_find_int(processes, pid);
-
-							//if I can't find pid_num in tree, don't do anything
-							if(pid_num == NULL) {
-
-							}//ends of if
-
-							//if I do find pid_num, delete node
-							jrb_delete_node(pid_num);
-
-							//if pid is equal to fv then break
-							if(pid == fv) {
-								break;
-							}//end of inner if
-						} //end of while
-					} //end of outer if
-				} //end of else
-
-				//free newargv
-				free(newargv);
-			} //end of while
-		} //end of if
-
-		/*traverse jrb tree and call wait*/
-		jrb_traverse(p, processes) {
-			wait(&stat_loc);
-		} //end of jrb_traverse 
-
-		//free jrb tree
-		jrb_free_tree(processes);
-	} //end of main
-
-	/*void pipe(JRB process, IS is->fields) {
+	for(i = 0; i < is->NF; i++) {
+		newargv[i] = is->fields[i];
+	}//end of for
 
 	//call pipe to set up two file descriptors
 	i = pipe(pipefd);
@@ -263,8 +208,8 @@ int main (int argc, char  *argv[]) {
 	//if it is unable to create pipe for interprocess communication
 	//print error and exit
 	if(i < 0) {
-	perror(is->fields[i+1]);
-	exit(1);
+		//perror(is->fields[i+1]);
+		exit(1);
 	}//end of if
 
 	//fork to create a new process and set to fv
@@ -276,46 +221,34 @@ int main (int argc, char  *argv[]) {
 	//check to see if fv is equal to 0
 	if(fv == 0) {
 
-	//check to see if it was successfully opened
-	//if not, we check to see if it was able to read from file
-	//if not, print error and exit
-	//then close file
-	//if(fd1 < 0) {
-	if(dup2(pipefd[1], 1) != 1) {
-	perror(is->fields[i+1]);
-	exit(1);
-	}//end of if
-	close(pipefd[1]);
-	close(pipefd[0]);
-	execlp(newargv, newargv[0]);
-	exit(1);
-	//}//end of if
+		//check to see if it was successfully opened
+		//if not, we check to see if it was able to read from file
+		//if not, print error and exit
+		//then close file
+		//if(fd1 < 0) {
+		if(dup2(pipefd[1], 1) != 1) {
+			//perror(is->fields[i+1]);
+			exit(1);
+		}//end of if
+		close(pipefd[1]);
+		close(pipefd[0]);
+		execvp(newargv[0], newargv);
+		exit(1);
+		//}//end of if
 
-	//check to see if it was successfully opened
-	//if not, check to see if it was able to write to file
-	//if not, print error and exit
-	//then close file
-	//if(fd2 < 0) {
-	if(dup2(pipefd[0], 0) != 0) {
-	perror(is->fields[i+1]);
-	exit(1);
-	}//end of if
-	close(pipefd[1]);
-	close(pipefd[0]);
-	execlp(newargv, newargv[0]);
-	exit(1);
-	//}//end of if
-
-	//check to see if it was successfully opened
-	//if not, check to see if it was able to write to file
-	//if not, print error and exit
-	//then close file
-	if(fd3 < 0) {
-	if(dup2(fd3, 1) != 1) {
-	perror(is->fields[i+1]);
-	exit(1);
-	} //end of if
-	close(fd3);
-	} //end of if
+		//check to see if it was successfully opened
+		//if not, check to see if it was able to write to file
+		//if not, print error and exit
+		//then close file
+		//if(fd2 < 0) {
+		if(dup2(pipefd[0], 0) != 0) {
+			//perror(is->fields[i+1]);
+			exit(1);
+		}//end of if
+		close(pipefd[1]);
+		close(pipefd[0]);
+		execvp(newargv[0], newargv);
+		exit(1);
+		//}//end of if
 	}//end of outer if*/
-	//}
+}//end of pipe_funct function 
