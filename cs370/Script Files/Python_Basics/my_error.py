1fce71afaa95a412d5a0bc8b4b3888df4a5d7e0a
Dodson, Terryl <dodsontd1@ornl.gov>
2020-12-09 16:02:31 -0500

    Cleaning everything up

--- cs370/Script Files/Python_Basics/my_error.py
+++
@@ -1,7 +0,0 @@
-## module my_error
-'''err(string). Prints 'string' and terminates program.'''
-import sys
-def err(string) :
-     print (string)
-     input('Press return to exit')
-     sys.exit()
--- cs370/test_numpy.py
+++
@@ -1,3 +0,0 @@
-from numpy import *
-a = arange(15).reshape(3, 5)
-print(a)
